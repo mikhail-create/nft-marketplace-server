09601b898d7f34fd5c5a73fd76b9492e29592907
Mikhail <leonovm143@gmail.com>
2023-08-08 15:53:34 +0600

    cors added

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = 5000;
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true
+  });
+  
   await app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
   });
